efa65b849dbfcdc082bca1a3351a8d0a4225f1dc
adumm <austin.dumm@learninga-z.com>
2020-12-04 08:09:57 -0500

    Initial Commit

---
+++ MealMatch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MealMatch
+//
+//  Created by Austin Dumm on 12/4/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MealMatch/MealMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealMatchApp.swift
+//  MealMatch
+//
+//  Created by Austin Dumm on 12/4/20.
+//
+
+import SwiftUI
+
+@main
+struct MealMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
